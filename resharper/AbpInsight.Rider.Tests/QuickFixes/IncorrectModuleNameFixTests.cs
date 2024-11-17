429f25c873495f1a6cbc9b90734201ef176d6d3c
Carbon <1149328035@qq.com>
2024-11-17 21:05:59 +0800

    add IncorrectModuleNameFixTests

---
+++ resharper/AbpInsight.Rider.Tests/QuickFixes/IncorrectModuleNameFixTests.cs
@@ -0,0 +1,26 @@
+﻿using AbpInsight.Services.QuickFixes;
+using JetBrains.ReSharper.FeaturesTestFramework.Intentions;
+using NUnit.Framework;
+
+namespace AbpInsight.Rider.Tests.QuickFixes;
+
+[TestAbpInsight]
+public class IncorrectModuleNameFixAvailabilityTests : QuickFixAvailabilityTestBase<IncorrectModuleNameQuickFix>
+{
+    protected override string RelativeTestDataPath => @"QuickFixes\IncorrectModuleName\Availability";
+
+    [Test]
+    public void IncorrectModuleName() => DoNamedTest();
+}
+
+[TestAbpInsight]
+public class IncorrectModuleNameFixTests : CSharpQuickFixTestBase<IncorrectModuleNameQuickFix>
+{
+    protected override string RelativeTestDataPath => @"QuickFixes\IncorrectModuleName";
+
+    [Test]
+    public void Test01() => DoNamedTest();
+
+    [Test]
+    public void Test02() => DoNamedTest();
+}
---
+++ resharper/AbpInsight.Rider.Tests/test/data/QuickFixes/IncorrectModuleName/Availability/IncorrectModuleName.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.Modularity;
+
+public class Test01 : AbpModule
+{
+}
+
+public class Test02Module : AbpModule
+{
+}
+
+public class Test02module : AbpModule
+{
+}
