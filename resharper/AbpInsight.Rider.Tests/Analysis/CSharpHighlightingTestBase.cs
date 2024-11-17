11222720fd2fc922ce1eb096874bce3f8f78e54d
Carbon <1149328035@qq.com>
2024-11-17 19:42:09 +0800

    update test case

---
+++ resharper/AbpInsight.Rider.Tests/Analysis/CSharpHighlightingTestBase.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.FeaturesTestFramework.Daemon;
+using JetBrains.ReSharper.Psi;
+
+namespace AbpInsight.Rider.Tests.Analysis;
+
+public abstract class CSharpHighlightingTestBase<T> : CSharpHighlightingTestBase
+{
+    protected override bool HighlightingPredicate(IHighlighting highlighting, IPsiSourceFile sourceFile,
+        IContextBoundSettingsStore settingsStore)
+    {
+        return highlighting is T;
+    }
+}
---
+++ resharper/AbpInsight.Rider.Tests/TestAbpInsightAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Projects;
+using JetBrains.Util.Dotnet.TargetFrameworkIds;
+using NuGet.Frameworks;
+using NuGet.Packaging.Core;
+
+namespace AbpInsight.Rider.Tests;
+
+public class TestAbpInsightAttribute(Version version)
+    : TestAspectAttribute, ITestTargetFrameworkIdProvider, ITestPackagesProvider, IReuseSolutionScopeAttribute
+{
+    private static readonly Version DefaultVersion = new(8, 0);
+
+    public TestAbpInsightAttribute() : this(DefaultVersion)
+    {
+    }
+
+    public TargetFrameworkId GetTargetFrameworkId() => TargetFrameworkId.Create(FrameworkConstants.CommonFrameworks.NetCoreApp20);
+
+    public IEnumerable<PackageDependency> GetPackages(TargetFrameworkId targetFrameworkId)
+    {
+        return GetPackageNames().Select(it => TestPackagesAttribute.ParsePackageDependency($"{it}/{version.Major}.{version.Minor}.0"));
+    }
+
+    public string ScopeName => version.ToString();
+
+
+    private static IEnumerable<string> GetPackageNames()
+    {
+        yield return "Volo.Abp.Core";
+    }
+
+    public bool Inherits => false;
+}
