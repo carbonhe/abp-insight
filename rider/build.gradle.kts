bdc6097ed70bda714bb69e22a7365ccf95ee4b89
Carbon <1149328035@qq.com>
2024-11-20 14:47:24 +0800

    release 0.0.2

--- rider/build.gradle.kts
+++ rider/build.gradle.kts
@@ -29,7 +29,7 @@ extra["rdLibDirectory"] = rdLibDirectory
 val repoRoot = projectDir.parentFile!!
 val backendRoot = File(repoRoot, "resharper")
 
-version = "0.0.1"
+version = "0.0.2"
 
 
 sourceSets {
