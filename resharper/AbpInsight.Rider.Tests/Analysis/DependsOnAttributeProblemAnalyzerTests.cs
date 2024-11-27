f362afbd17c25c7cc147fb0fda6ec7399d9746c1
Carbon <1149328035@qq.com>
2024-11-27 23:51:48 +0800

    add analyzer for inspecting DependsOnAttribute's parameters
    # Conflicts:
    #	resharper/AbpInsight/Resources/Strings.Designer.cs
    #	resharper/AbpInsight/Resources/Strings.resx

---
+++ resharper/AbpInsight.Rider.Tests/Analysis/DependsOnAttributeProblemAnalyzerTests.cs
@@ -0,0 +1,11 @@
+﻿using AbpInsight.Daemon.Stages.Highlightings;
+using NUnit.Framework;
+
+namespace AbpInsight.Rider.Tests.Analysis;
+
+[TestAbpInsight]
+public class DependsOnAttributeProblemAnalyzerTests : CSharpHighlightingTestBase<CSharpAbpInsightHighlightingBase>
+{
+    [Test]
+    public void DependsOnAttributeProblemAnalyzer() => DoNamedTest();
+}
--- resharper/AbpInsight/Resources/Strings.Designer.cs
+++ resharper/AbpInsight/Resources/Strings.Designer.cs
@@ -65,6 +65,7 @@ namespace AbpInsight.Resources
     public static string ModuleTypeMustBePublic_Message => ResourceManager.GetString("ModuleTypeMustBePublic_Message");
     public static string DependencyTypeMustBePublic_Message => ResourceManager.GetString("DependencyTypeMustBePublic_Message");
     public static string DependencyImplementsMultipleLifetimes_Message => ResourceManager.GetString("DependencyImplementsMultipleLifetimes_Message");
+    public static string DependsOnNonAbpModuleType_Message => ResourceManager.GetString("DependsOnNonAbpModuleType_Message");
     public static string AddAbpWidget_Description => ResourceManager.GetString("AddAbpWidget_Description");
   }
 }
